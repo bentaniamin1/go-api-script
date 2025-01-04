17734050dfeb7976ac2d33632eb11c1c5bd38aac
bentaniamin1 <bentani.amin2000@gmail.com>
2025-01-04 20:28:14 +0000

    Fonc: add method PUT and Delete

--- src/go-api-script/main.go
+++ src/go-api-script/main.go
@@ -17,6 +17,13 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Kind of Blue", Artist: "Miles Davis", Price: 29.99},
+	{ID: "5", Title: "A Love Supreme", Artist: "John Coltrane", Price: 34.99},
+	{ID: "6", Title: "The Shape of Jazz to Come", Artist: "Ornette Coleman", Price: 25.99},
+	{ID: "7", Title: "Out to Lunch!", Artist: "Eric Dolphy", Price: 31.99},
+	{ID: "8", Title: "Mingus Ah Um", Artist: "Charles Mingus", Price: 27.99},
+	{ID: "9", Title: "Time Out", Artist: "The Dave Brubeck Quartet", Price: 22.99},
+	{ID: "10", Title: "Somethin' Else", Artist: "Cannonball Adderley", Price: 24.99},
 }
 
 func getAlbums(c *gin.Context) {
@@ -46,11 +53,51 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func updateAlbums(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range albums {
+		if a.ID == id {
+			var updatedAlbum album
+
+			if err := c.BindJSON(&updatedAlbum); err != nil {
+				return
+			}
+
+			albums[i] = updatedAlbum
+			c.IndentedJSON(http.StatusOK, updatedAlbum)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+func deleteAlbums(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range albums {
+		if a.ID == id {
+			var deletedAlbum album
+
+			if err := c.BindJSON(&deletedAlbum); err != nil {
+				return
+			}
+
+			// albums[i] = deletedAlbum
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusOK, albums)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albumByID/:id", getAlbumByID)
 	router.POST("/postAlbums", postAlbums)
+	router.PUT("/updateAlbums/:id", updateAlbums)
+	router.DELETE("/deleteAlbums/:id", deleteAlbums)
 
 	router.Run("localhost:8080")
 }
